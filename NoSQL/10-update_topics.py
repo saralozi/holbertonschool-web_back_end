17a990c111af358379089496f3b721cb8c5d739e
saralozi <saralozi08@gmail.com>
2025-11-19 23:37:26 +0100

    Adding solution for task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+"""A script that changes all topics of a school document based on the name:"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """A function that updates a document in a collection"""
+
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
